f6ec5463a88189305f5f8740a787b3773c5fa427
Rahul Sheelavantar <rahulsheelavantar@gmail.com>
2022-11-11 09:37:57 +0530

    Day-4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <AllRoutes />
     </div>
   );
 }
